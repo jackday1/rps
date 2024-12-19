8b35696e321643cd86386489da4ee582027941f0
jackday1 <jack.nguyen@weareday.one>
2024-12-19 21:36:47 +0700

    init

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,151 @@ import io from 'socket.io-client';
 
 const socket = io('http://localhost:8888');
 
+const BarGraph = ({ counts }) => {
+  const total = counts.rock + counts.paper + counts.scissors;
+  const rockPercent = counts.rock / total;
+  const paperPercent = counts.paper / total;
+  const scissorsPercent = 1 - rockPercent - paperPercent;
+
+  return (
+    <div>
+      <div
+        style={{
+          width: '100%',
+          height: '20px',
+          display: 'flex',
+        }}
+      >
+        <div
+          style={{
+            height: '100%',
+            width: `${rockPercent * 100}%`,
+            backgroundColor: '#616d77',
+            transition: 'all ease 0.2s',
+            position: 'relative',
+          }}
+        >
+          {counts.rock && (
+            <div
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                transform: 'translateY(-100%)',
+                display: 'flex',
+                justifyContent: 'center',
+                padding: 8,
+              }}
+            >
+              <div
+                style={{
+                  padding: '2px 4px',
+                  borderRadius: 4,
+                  boxShadow: '0px 0px 50px 0px rgba(0,0,0,0.75)',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <img src="rock.png" alt="rock" width={16} />
+                <span style={{ fontWeight: 600, fontSize: 13 }}>
+                  {counts.rock}
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+        <div
+          style={{
+            height: '100%',
+            width: `${paperPercent * 100}%`,
+            backgroundColor: '#e8ba5c',
+            transition: 'all ease 0.2s',
+            position: 'relative',
+          }}
+        >
+          {counts.paper && (
+            <div
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                transform: 'translateY(-100%)',
+                display: 'flex',
+                justifyContent: 'center',
+                padding: 8,
+              }}
+            >
+              <div
+                style={{
+                  padding: '2px 4px',
+                  borderRadius: 4,
+                  boxShadow: '0px 0px 50px 0px rgba(0,0,0,0.75)',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <img src="paper.png" alt="rock" width={16} />
+                <span style={{ fontWeight: 600, fontSize: 13 }}>
+                  {counts.paper}
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+        <div
+          style={{
+            height: '100%',
+            width: `${scissorsPercent * 100}%`,
+            backgroundColor: '#fd5d72',
+            transition: 'all ease 0.2s',
+            position: 'relative',
+          }}
+        >
+          {counts.scissors && (
+            <div
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                transform: 'translateY(-100%)',
+                display: 'flex',
+                justifyContent: 'center',
+                padding: 8,
+              }}
+            >
+              <div
+                style={{
+                  padding: '2px 4px',
+                  borderRadius: 4,
+                  boxShadow: '0px 0px 50px 0px rgba(0,0,0,0.75)',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <img src="scissors.png" alt="rock" width={16} />
+                <span style={{ fontWeight: 600, fontSize: 13 }}>
+                  {counts.scissors}
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   const canvasRef = useRef(null);
   const [gameState, setGameState] = useState([]);
+  const [winner, setWinner] = useState(null);
+  const [timeLeft, setTimeLeft] = useState('');
 
   const rockImg = useRef(null);
   const paperImg = useRef(null);
@@ -19,12 +161,18 @@ const App = () => {
 
   useEffect(() => {
     // Listen for game state updates from the server
-    socket.on('gameState', (state) => {
-      setGameState(state);
+    socket.on('gameState', ({ gameState, timeLeft }) => {
+      setGameState(gameState);
+      setTimeLeft(Math.floor(Math.max(timeLeft, 0) / 1000));
+    });
+
+    socket.on('gameOver', ({ winner }) => {
+      setWinner(winner); // Display the winner
     });
 
     return () => {
       socket.off('gameState');
+      socket.off('gameOver');
     };
   }, []);
 
@@ -66,15 +214,37 @@ const App = () => {
     prevGameState.current = gameState;
   }, [gameState]);
 
+  const counts = gameState.reduce(
+    (result, item) => {
+      result[item.type]++;
+      return result;
+    },
+    { rock: 0, paper: 0, scissors: 0 }
+  );
+
   return (
-    <div style={{ background: 'black' }}>
-      <h1>Rock, Paper, Scissors Battle</h1>
+    <div
+      style={{
+        width: '100vw',
+        overflow: 'auto',
+        background: 'white',
+        padding: 16,
+      }}
+    >
+      <p style={{ fontSize: 40, fontWeight: 600, textAlign: 'center' }}>
+        {timeLeft}
+      </p>
+      <BarGraph counts={counts} />
       <canvas
         ref={canvasRef}
         width={800}
-        height={600}
-        style={{ border: '1px solid white' }}
+        height={800}
+        style={{
+          width: '100%',
+          backgroundColor: '#ddd',
+        }}
       />
+      {winner && <h3>{winner.toUpperCase()} Wins!</h3>}
 
       <img
         ref={rockImg}
--- backend/index.js
+++ backend/index.js
@@ -7,108 +7,166 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: '*' } });
 
-const PORT = 8888;
-
 const width = 800;
-const height = 600;
+const height = 800;
 const itemCount = 25;
 const itemTypes = ['rock', 'paper', 'scissors'];
-const speed = 2;
+const speed = 1.5;
+const timer = 300 * 1000;
+const size = 20;
 
-// Helper function to generate random velocity
-function getRandomVelocity() {
-  return {
-    x: (Math.random() * 2 - 1) * speed,
-    y: (Math.random() * 2 - 1) * speed,
+const main = () => {
+  // Helper function to generate random velocity
+  const getRandomVelocity = () => {
+    return {
+      x: (Math.random() * 2 - 1) * speed,
+      y: (Math.random() * 2 - 1) * speed,
+    };
   };
-}
 
-// Helper function to get initial positions by clusters
-function getInitialPosition(type) {
-  const clusterPadding = 100;
-  if (type === 'rock') {
-    return {
-      x: Math.random() * (width / 3 - clusterPadding) + clusterPadding / 2,
-      y: Math.random() * (height - clusterPadding) + clusterPadding / 2,
-    };
-  } else if (type === 'paper') {
-    return {
-      x:
-        Math.random() * (width / 3 - clusterPadding) +
-        width / 3 +
-        clusterPadding / 2,
-      y: Math.random() * (height - clusterPadding) + clusterPadding / 2,
-    };
-  } else {
-    return {
-      x:
-        Math.random() * (width / 3 - clusterPadding) +
-        (2 * width) / 3 +
-        clusterPadding / 2,
-      y: Math.random() * (height - clusterPadding) + clusterPadding / 2,
-    };
-  }
-}
+  const randomInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
 
-// Initialize game state
-let gameState = [];
-itemTypes.forEach((type) => {
-  for (let i = 0; i < itemCount; i++) {
-    const position = getInitialPosition(type);
-    const velocity = getRandomVelocity();
-    gameState.push({
-      type,
-      x: position.x,
-      y: position.y,
-      vx: velocity.x,
-      vy: velocity.y,
-    });
-  }
-});
+  // Helper function to get initial positions by clusters
+  const getInitialPosition = (type) => {
+    if (type === 'rock') {
+      return {
+        x: 250,
+        y: 150,
+      };
+      return {
+        x: randomInRange(0, width / 3),
+        y: randomInRange(0, height / 3),
+      };
+    } else if (type === 'paper') {
+      return {
+        x: 700,
+        y: 450,
+      };
+      return {
+        x: randomInRange((2 / 3) * width, width),
+        y: randomInRange(height / 3, (2 / 3) * height),
+      };
+    } else {
+      return {
+        x: 150,
+        y: 650,
+      };
+      return {
+        x: randomInRange(width / 3, (2 / 3) * width),
+        y: randomInRange((2 / 3) * height, height),
+      };
+    }
+  };
 
-setInterval(() => {
-  // Update positions and handle edge of canvas collisions
-  gameState.forEach((item) => {
-    item.x += item.vx;
-    item.y += item.vy;
-
-    // Bounce off walls
-    if (item.x <= 0 || item.x >= width) item.vx *= -1;
-    if (item.y <= 0 || item.y >= height) item.vy *= -1;
-
-    // Simulate simple "touch" collision detection
-    gameState.forEach((otherItem) => {
-      if (
-        item !== otherItem &&
-        Math.abs(item.x - otherItem.x) < 20 &&
-        Math.abs(item.y - otherItem.y) < 20
-      ) {
-        // Check winning condition
-        if (
-          (item.type === 'rock' && otherItem.type === 'scissors') ||
-          (item.type === 'scissors' && otherItem.type === 'paper') ||
-          (item.type === 'paper' && otherItem.type === 'rock')
-        ) {
-          otherItem.type = item.type; // Loser turns into the winner's type
-
-          // Change the velocity of both items on collision
-          const newVelocity1 = getRandomVelocity();
-          const newVelocity2 = getRandomVelocity();
-
-          item.vx = newVelocity1.x;
-          item.vy = newVelocity1.y;
-          otherItem.vx = newVelocity2.x;
-          otherItem.vy = newVelocity2.y;
-        }
-      }
-    });
+  // Initialize game state
+  let startTime = Date.now();
+  let gameState = [];
+  itemTypes.forEach((type) => {
+    for (let i = 0; i < itemCount; i++) {
+      const position = getInitialPosition(type);
+      const velocity = getRandomVelocity();
+      gameState.push({
+        type,
+        x: position.x,
+        y: position.y,
+        vx: velocity.x,
+        vy: velocity.y,
+      });
+    }
   });
 
-  // Emit updated game state
-  io.emit('gameState', gameState);
-}, 1000 / 30);
+  const gameLoop = setInterval(() => {
+    const now = Date.now();
+    const timeLeft = timer + startTime - now;
+
+    // Update positions and handle edge of canvas collisions
+    gameState.forEach((item) => {
+      item.x += item.vx;
+      item.y += item.vy;
+
+      // Bounce off walls
+      if (item.x - size / 2 <= 0 || item.x + size / 2 >= width) item.vx *= -1;
+      if (item.y - size / 2 <= 0 || item.y + size / 2 >= height) item.vy *= -1;
+
+      // Simulate simple "touch" collision detection
+      gameState.forEach((otherItem) => {
+        if (
+          item !== otherItem &&
+          Math.abs(item.x - otherItem.x) < size &&
+          Math.abs(item.y - otherItem.y) < size
+        ) {
+          // Check winning condition
+          if (
+            (item.type === 'rock' && otherItem.type === 'scissors') ||
+            (item.type === 'scissors' && otherItem.type === 'paper') ||
+            (item.type === 'paper' && otherItem.type === 'rock')
+          ) {
+            otherItem.type = item.type; // Loser turns into the winner's type
+
+            // Change the velocity of both items on collision
+            const newVelocity1 = getRandomVelocity();
+            const newVelocity2 = getRandomVelocity();
+
+            item.vx = newVelocity1.x;
+            item.vy = newVelocity1.y;
+            otherItem.vx = newVelocity2.x;
+            otherItem.vy = newVelocity2.y;
+          }
+        }
+      });
+    });
+
+    // Emit updated game state
+    io.emit('gameState', { gameState, timeLeft });
+
+    const counts = {
+      rock: 0,
+      paper: 0,
+      scissors: 0,
+    };
+    gameState.map((item) => counts[item.type]++);
+
+    if (
+      counts.rock === itemCount * 3 ||
+      counts.paper === itemCount * 3 ||
+      counts.scissors === itemCount * 3
+    ) {
+      io.emit('gameOver', {
+        winner:
+          counts.rock > 0 ? 'rock' : counts.paper > 0 ? 'paper' : 'scissors',
+      });
+      clearInterval(gameLoop);
+
+      setTimeout(main, 5000);
+    }
+
+    if (timeLeft <= 0) {
+      let max = counts.rock;
+      let winner = 'rock';
+      if (counts.paper > max) {
+        max = counts.paper;
+        winner = 'paper';
+      }
+
+      if (counts.scissors > max) {
+        max = counts.scissors;
+        winner = 'scissors';
+      }
+      io.emit('gameOver', {
+        winner,
+      });
+      clearInterval(gameLoop);
+
+      setTimeout(main, 5000);
+    }
+  }, 1000 / 30);
+};
+
+main();
 
 // Start server
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(process.env.PORT || 8888, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8888}`);
 });
